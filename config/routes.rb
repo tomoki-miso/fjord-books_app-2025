c642e6065e5607b0148d056223516aeabc08eab5
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-13 17:11:02 +0900

    feat: ルートパスを「books#index」に設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations' 
   }
+  root "books#index"   
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
