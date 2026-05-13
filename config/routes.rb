d2e39e299cf20220b927591ae908a9a3a031d7d5
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-13 16:57:04 +0900

    feat: ユーザー登録用のDeviseコントローラーを指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations' 
+  }
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -13,4 +15,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  
 end
