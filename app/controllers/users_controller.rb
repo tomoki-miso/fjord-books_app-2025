bf1f041c393abb863df3aa234a7ed15abd42a792
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-14 14:30:59 +0900

    feat: ユーザー管理機能を追加し、ユーザー一覧、詳細表示、ヘルパーを実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+  def index
+    @users = User.order(:id)
+  end
+
+  def show; end
+
+  def set_user
+    @user = User.find(params.expect(:id))
+  end
+
+  def user_params
+    params.expect(user: %i[name email post_code address bio]).require(:user)
+  end
+end
