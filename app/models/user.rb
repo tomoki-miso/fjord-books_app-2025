45d6d266428cd1b4336f28ad387744288b16b0eb
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-15 15:16:12 +0900

    feat: active_storage_validationsを追加し、ユーザーのアバターのバリデーションを強化

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
+  validates :avatar, content_type: { in: %w[image/jpeg image/png] }
 end
