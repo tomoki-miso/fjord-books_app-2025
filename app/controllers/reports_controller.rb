3f6bc2870720dfebb172472bdceaa403828534d0
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-15 18:12:20 +0900

    feat : 日報機能基盤

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+  def index
+    @reports = Report.order(:id).page(params[:page])
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @report.update(report_params)
+        format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
+        format.json { render :show, status: :ok, location: @report }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @report.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :content)
+  end
+end
