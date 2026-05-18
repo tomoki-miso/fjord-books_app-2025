7081e9b1c646b70d51d4dd14b0f5e7e9729a2d7a
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-18 16:27:31 +0900

    chore: 静的解析対応

--- test/controllers/report_controller_test.rb
+++ test/controllers/report_controller_test.rb
@@ -1,27 +1,29 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ReportControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get report_index_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get report_new_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get report_show_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get report_edit_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get report_create_url
     assert_response :success
   end
