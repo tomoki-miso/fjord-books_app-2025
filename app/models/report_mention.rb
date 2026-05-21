830e583adf3f03e58539c6b1bf425a22585008e5
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-21 13:29:12 +0900

    feat: report_mentionモデルの関連付けを修正し、逆関連を削除

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -2,5 +2,5 @@
 
 class ReportMention < ApplicationRecord
   belongs_to :report, inverse_of: :report_mentions
-  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :report_mentions
+  belongs_to :mentioned_report, class_name: 'Report'
 end
