10c1e509399ae515c9a8371c0cbca6e52cf3b5a2
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-27 10:29:05 +0900

    refactor: ユーザー名表示ロジックをメソッド化し、コメントとレポートでの利用を統一

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   end
   has_many :reports, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def display_name
+    name.presence || email
+  end
 end
