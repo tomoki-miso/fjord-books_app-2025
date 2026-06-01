e38b5a2433d56b6741d41bb1d24d17705c43dc17
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-06-01 11:29:25 +0900

    remove: avater 関係のテスト削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,30 +3,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'avatar can be attached' do
-    user = users(:one)
-
-    user.avatar.attach(
-      io: File.open(Rails.root.join('test/fixtures/files/avatar.png')),
-      filename: 'avatar.png',
-      content_type: 'image/png'
-    )
-
-    assert user.avatar.attached?
-  end
-
-  test 'avatar thumb variant is defined' do
-    user = users(:one)
-
-    user.avatar.attach(
-      io: File.open(Rails.root.join('test/fixtures/files/avatar.png')),
-      filename: 'avatar.png',
-      content_type: 'image/png'
-    )
-
-    assert user.avatar.variant(:thumb)
-  end
-
   test 'name_or_email returns name when name is present' do
     assert_equal 'One', users(:one).name_or_email
   end
